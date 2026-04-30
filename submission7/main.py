bfa1172705e390c0c096ca8f55c84bc4e8fc253e
shlstd <shelest.dasha.24680@gmail.com>
2026-04-30 16:32:26 +0300

    Update main.py

--- submission7/main.py
+++ submission7/main.py
@@ -12,3 +12,26 @@ input_file = "input.txt"
 # Формат даних у файлі (варіант 9): "ім'я:вік" у кожному рядку
 
 # Реалізуйте завдання тут
+try:
+    a = float(input("Введіть число: "))
+
+    if a < 0:
+        raise ValueError("Введено від’ємне число!")
+
+except ValueError as error:
+    print(f"Помилка: {error}")
+
+    with open(error_file, "a", encoding="utf-8") as log_file:
+        log_file.write(f"Помилка: {error}\n")
+
+else:
+    print(f"Число коректне: {a}")
+
+    with open(error_file, "a", encoding="utf-8") as log_file:
+        log_file.write(f"Успішне введення: {a}\n")
+
+finally:
+    print("Програма завершила виконання.")
+
+    with open(error_file, "a", encoding="utf-8") as log_file:
+        log_file.write("Завершення програми\n")
